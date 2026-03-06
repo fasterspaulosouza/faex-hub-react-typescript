d62d71b87ed24bf73fdf8265f1fd92a682f11b6d
Paulo <paulos@portasabertas.org.br>
2026-03-05 21:18:41 -0300

    add private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login/Login";
-import Cadastro from "./components/Cadastro/Cadastro";
-import Configuracao from "./components/Configuracao/Configuracao";
-import Home from "./components/Home/Home";
-import Layout from "./components/Layout/Layout";
-import Friends from "./components/Friends/Friends";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Cadastro from './components/Cadastro/Cadastro';
+import Configuracao from './components/Configuracao/Configuracao';
+import Home from './components/Home/Home';
+import Layout from './components/Layout/Layout';
+import Friends from './components/Friends/Friends';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+      <AuthProvider>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/cadastro' element={<Cadastro />} />
 
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/amigos" element={<Friends />} />
-          <Route path="/configuracao" element={<Configuracao />} />
-        </Route>
-      
-      </Routes>
+          <Route element={<PrivateRoute />}>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/amigos' element={<Friends />} />
+              <Route path='/configuracao' element={<Configuracao />} />
+            </Route>
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
